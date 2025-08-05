cdf1be39983a5d8dede584610996e4115e264d52
Bindu2905 <bindumadhurivandamsetty@gmail.com>
2025-08-05 13:46:19 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-// React entry point that renders the main App component
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
